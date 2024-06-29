b6489a3c9f88ad57cd8bd9e187d6c8e442a285c5
majedur rahman <youremail@yourdomain.com>
2024-06-30 00:12:21 +0600

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express, {  } from "express";
 import gobalErrorHander from "./middlewares/gobalErrorHandler";
 import userRouter from './user/userRouter';
 import bookRouter from "./book/bookRouter";
+import cors from 'cors'
 const app = express();
 app.use(express.json())
 
-
+app.use(cors({
+origin:"http://localhost:3000/"
+}))
 app.use('/api/users', userRouter)
 app.use("/api/books", bookRouter);
 app.use(gobalErrorHander);
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -4,7 +4,7 @@ import { createAccount, loginUser } from './userController'
 const userRouter = express.Router()
 userRouter.post('/register', createAccount)
 userRouter.get("/", (req:Request, res:Response) => {
-    res.send("Hello World")
+    res.send("Hello World 34aa")
  
 });
 userRouter.post("/login", loginUser);
