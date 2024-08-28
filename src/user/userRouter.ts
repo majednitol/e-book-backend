0ff832344aca31ddfe5d17b829b890e5e4993d99
majedur rahman <youremail@yourdomain.com>
2024-08-29 02:03:12 +0600

    done

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,11 +1,11 @@
-import express, {  } from 'express'
+import express, { Request, Response } from 'express'
 import { createAccount, loginUser } from './userController'
 
 const userRouter = express.Router()
 userRouter.post('/register', createAccount)
-// userRouter.get("/", (req:Request, res:Response) => {
-//     res.send("Hello World 34aa")
+userRouter.get("/", (req:Request, res:Response) => {
+    res.send("Hello World!")
  
-// });
+});
 userRouter.post("/login", loginUser);
 export default userRouter
